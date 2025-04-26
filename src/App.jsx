927d7e6afeb620a84b465dea2895c861ecdb94e8
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-04-26 11:37:03 -0300

    Instalando o VanillaTilt no  Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { VanillaTiltComponent } from './components/VanillaTilt/VanillaTilt' // importa aqui!
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      {/* Aqui aplicamos o VanillaTilt ao h1 */}
+      <VanillaTiltComponent options={{ scale: 1.1, speed: 1000, max: 25 }}>
+        <h1>Vite + React</h1>
+      </VanillaTiltComponent>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
