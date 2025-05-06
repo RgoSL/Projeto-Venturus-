4bb1f2b797c170a3eb5506b2c8bd075432b26705
lipedeoliveira <felipe.vieira.7721@gmail.com>
2025-05-06 17:01:27 -0300

    Subindo uma coisinhas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
     import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
     import { Forms } from "./components/Forms/forms.jsx";
     import ExibirUsuarios from "./pages/ExibirCards/ExibirUsuarios.jsx";
-
+    import { SobreNos } from "./components/Sobrenos/SobreNos.jsx";
     function App() {
     return (
         <Router>
         <Routes>
             <Route path="/" element={<Forms />} />
             <Route path="/usuarios" element={<ExibirUsuarios />} />
+            <Route path="/sobrenos" element={<SobreNos />} />
         </Routes>
         </Router>
     );
